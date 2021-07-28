a77e8e53ee36688baec8cdd1fa27999d3eba3c8e
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-28 21:23:52 +1000

    Remove all `expect(...)` used in mod sync
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/sync.rs
+++ src/sync.rs
@@ -10,14 +10,14 @@ pub use state_storage::*;
 
 #[cfg(not( any(feature = "async_tokio", features = "permutation_testing") ))]
 mod state_sync {
-    pub use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+    pub use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard, LockResult};
 
-    pub fn read<T: ?Sized>(rwlock: &RwLock<T>) -> RwLockReadGuard<'_, T> {
-        rwlock.read().expect("Failed to lock std::sync::RwLock for reading")
+    pub fn read<T: ?Sized>(rwlock: &RwLock<T>) -> LockResult<RwLockReadGuard<'_, T>> {
+        rwlock.read()
     }
 
-    pub fn write<T: ?Sized>(rwlock: &RwLock<T>) -> RwLockWriteGuard<'_, T> {
-        rwlock.write().expect("Failed to lock std::sync::RwLock for writing")
+    pub fn write<T: ?Sized>(rwlock: &RwLock<T>) -> LockResult<RwLockWriteGuard<'_, T>> {
+        rwlock.write()
     }
 
     #[macro_export]
@@ -38,13 +38,14 @@ mod state_sync {
 #[cfg(feature = "async_tokio")]
 mod state_sync {
     pub use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+    pub type LockResult<T> = Result<T, ()>;
 
-    pub async fn read<T: ?Sized>(rwlock: &RwLock<T>) -> RwLockReadGuard<'_, T> {
-        rwlock.read()
+    pub async fn read<T: ?Sized>(rwlock: &RwLock<T>) -> LockResult<RwLockReadGuard<'_, T>> {
+        Ok(rwlock.read())
     }
 
-    pub async fn write<T: ?Sized>(rwlock: &RwLock<T>) -> RwLockWriteGuard<'_, T> {
-        rwlock.write()
+    pub async fn write<T: ?Sized>(rwlock: &RwLock<T>) -> LockResult<RwLockWriteGuard<'_, T>> {
+        Ok(rwlock.write())
     }
 
     #[macro_export]
@@ -65,14 +66,14 @@ mod state_sync {
 #[cfg(feature = "permutation_testing")]
 #[macro_use]
 mod state_sync {
-    pub use loom::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+    pub use loom::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard, LockResult};
 
-    pub fn read<T: ?Sized>(rwlock: &RwLock<T>) -> RwLockReadGuard<'_, T> {
-        rwlock.read().expect("Failed to lock std::sync::RwLock for reading")
+    pub fn read<T: ?Sized>(rwlock: &RwLock<T>) -> LockResult<RwLockReadGuard<'_, T>> {
+        rwlock.read()
     }
 
-    pub fn write<T: ?Sized>(rwlock: &RwLock<T>) -> RwLockWriteGuard<'_, T> {
-        rwlock.write().expect("Failed to lock std::sync::RwLock for writing")
+    pub fn write<T: ?Sized>(rwlock: &RwLock<T>) -> LockResult<RwLockWriteGuard<'_, T>> {
+        rwlock.write()
     }
 
     macro_rules! read {
