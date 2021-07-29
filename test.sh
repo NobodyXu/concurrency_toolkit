37aabf61690848ca9467e544351987dc9aa70d17
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-29 17:06:27 +1000

    Improve test.sh: Run test under debug and release mode
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 
 for feature in default async_tokio permutation_testing; do
     cargo test --no-default-features --features $feature
+    cargo test --release --no-default-features --features $feature
 done
