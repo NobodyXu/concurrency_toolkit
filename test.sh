3249ca528efbd13d13709c9a9ea8dd5874f67471
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-02 01:08:39 +1000

    Improve test.sh so that it can be called inside workspace
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -ex
 
+cd $(dirname `realpath $0`)
+
 for feature in default async_tokio permutation_testing; do
     cargo test --no-default-features --features $feature
     cargo test --release --no-default-features --features $feature
