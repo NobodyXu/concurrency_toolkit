6e907cf2b4c96f055ab825ea6463d115f5f7a2a1
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-02 16:09:46 +1000

    Fix `maybe_async` not found when using `test` from another crate
    Also bump minor ver no for both crates for bug fix
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- proc_macro_test/src/lib.rs
+++ proc_macro_test/src/lib.rs
@@ -4,7 +4,7 @@ use quote::quote;
 
 /// Automatically start async runtime or call `loom::model` if required:
 ///
-/// ```no_run
+/// ```ignore
 /// #[concurrency_toolkit::test]
 /// fn test() {
 ///     // ...
@@ -15,6 +15,8 @@ use quote::quote;
 /// to not be declared as `async` due to implementation detail
 /// (`syn` doesn't provides an easy way to parse `async function), but it still can
 /// remove `async`-related keywords just like `maybe_async::maybe_async`.
+///
+/// **NOTE that this macro requires `concurrency_toolkit` to be imported**
 #[proc_macro_attribute]
 pub fn test(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // Construct a representation of Rust code as a syntax tree
@@ -34,11 +36,17 @@ pub fn test(_attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     };
 
+    #[cfg(not(feature = "async_tokio"))]
     let expanded = quote! {
-        #[concurrency_toolkit::maybe_async::test(
-            not(feature = "async_tokio"),
-            async(feature = "async_tokio", concurrency_toolkit::tokio::test),
-        )]
+        #[test]
+        #[concurrency_toolkit::maybe_async::must_be_sync]
+        # vis # sig # block
+    };
+
+    #[cfg(feature = "async_tokio")]
+    let expanded = quote! {
+        #[concurrency_toolkit::tokio::test]
+        #[concurrency_toolkit::maybe_async::must_be_async]
         # vis async # sig # block
     };
 
