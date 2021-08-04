7b4492b8855ad68d7773569bbbbf7ce6bc2bad2e
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-04 19:49:55 +1000

    `export RUST_BACKTRACE=1` in test.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 cd $(dirname `realpath $0`)
 
+export RUST_BACKTRACE=1
+
 for feature in default async_tokio permutation_testing; do
     cargo test --no-default-features --features $feature
     cargo test --release --no-default-features --features $feature
