559f21b1b2e76faa25eed7b4aa004693af250fbc
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-04 19:56:13 +1000

    Allow passing additional args to `cargo test` in test.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ cd $(dirname `realpath $0`)
 export RUST_BACKTRACE=1
 
 for feature in default async_tokio; do
-    cargo test --no-default-features --features $feature
-    cargo test --release --no-default-features --features $feature
+    cargo test --no-default-features --features $feature $@
+    cargo test --release --no-default-features --features $feature $@
 done
 
-cargo test --release --no-default-features --features permutation_testing
+cargo test --release --no-default-features --features permutation_testing $@
